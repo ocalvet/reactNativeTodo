b0a12da9816d84021feccbd5d1aa3f048cb4278c
Ovidio R. Calvet <calveto@gmail.com>
2016-03-03 20:51:19 -0500

    Added a list of todo items to disply

--- components/common/TodoList.js
+++ components/common/TodoList.js
@@ -17,15 +17,44 @@ class TodoList extends Component {
     super(props);
   }
 
+  renderTodoItem(todo) {
+    return (
+      <Text>{todo.title}</Text>
+    )
+  }
+
   render() {
+    var list = [
+      { title: 'Todo Item 1', description: 'title 1 description', lastUpdated: new Date() },
+      { title: 'Todo Item 2', description: 'title 1 description', lastUpdated: new Date() },
+      { title: 'Todo Item 3', description: 'title 1 description', lastUpdated: new Date() },
+      { title: 'Todo Item 4', description: 'title 1 description', lastUpdated: new Date() },
+      { title: 'Todo Item 5', description: 'title 1 description', lastUpdated: new Date() },
+      { title: 'Todo Item 6', description: 'title 1 description', lastUpdated: new Date() },
+      { title: 'Todo Item 7', description: 'title 1 description', lastUpdated: new Date() },
+      { title: 'Todo Item 8', description: 'title 1 description', lastUpdated: new Date() },
+      { title: 'Todo Item 9', description: 'title 1 description', lastUpdated: new Date() },
+      { title: 'Todo Item 10', description: 'title 1 description', lastUpdated: new Date() },
+      { title: 'Todo Item 11', description: 'title 1 description', lastUpdated: new Date() },
+      { title: 'Todo Item 12', description: 'title 1 description', lastUpdated: new Date() },
+      { title: 'Todo Item 13', description: 'title 1 description', lastUpdated: new Date() },
+      { title: 'Todo Item 14', description: 'title 1 description', lastUpdated: new Date() },
+      { title: 'Todo Item 15', description: 'title 1 description', lastUpdated: new Date() }
+    ];
+
+    var ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+
+    var clonedList = ds.cloneWithRows(list);
+
     return (
       <View
         style={{
           flex: 1,
-          justifyContent: 'flex-start',
-          paddingTop: 80
+          justifyContent: 'flex-start'
         }}>
-        <Text>Todo List</Text>
+        <ListView
+          dataSource={clonedList}
+          renderRow={this.renderTodoItem.bind(this)} />
       </View>
     );
   }
